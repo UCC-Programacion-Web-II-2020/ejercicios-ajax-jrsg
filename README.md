7d85fbe1cac34db86ec61552599befead43b0961
José Rubén Sánchez González <jrsg@outlook.com>
2020-10-29 20:35:12 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Instrucciones
+
+Los archivos de la práctica son:
+
+1. index.html - la descripción de los ejercicios.
+2. estilos.css - hoja de estilos.
+3. ejercicios.js - para la solución de los 3 ejercicios, lado cliente.
+4. paises.php - script para el ejercicio 1.
+5. codigos.php - script para el ejercicio 2.
+6. municipios.php - script para el ejercicio 3.
+7. sql/codigos_postales.sql - script parar crear una tabla con los códigos postales de Veracruz.
+8. sql/municipios.sql - script para crear una tabla con los municipios de México.
---
+++ paises.php
@@ -0,0 +1,23 @@
+<?php
+$paises = array('Afganistán', 'Albania', 'Alemania', 'Andorra', 'Angola', 'de Anguilla', 'Antártida', 'Antigua y Barbuda', 'Antillas Neerlandesas',
+    'Arabia Saudí', 'Argelia', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaiyán', 'Bahamas', 'Bahráin', 'Bangladesh', 'Barbados',
+    'Bélgica', 'Belice', 'Benín', 'Bermudas', 'Bielorrusia', 'Bolivia', 'Bosnia-Herzegovina', 'Botsuana', 'Brasil', 'Brunei Darussalam', 'Bulgaria',
+    'Burkina Faso', 'Burundi', 'Bután', 'Cabo Verde', 'Camboya', 'Camerún', 'Canadá', 'Chad', 'Chile', 'China', 'Chipre', 'Ciudad Vaticano',
+    'Colombia', 'Comoras', 'Corea del Norte', 'Corea del Sur', 'Costa de Marfil', 'Costa Rica', 'Croacia', 'Cuba', 'Islas Marshall', 'Dinamarca',
+    'Dominica', 'Emiratos Árabes Unidos', 'Ecuador', 'Estados Unidos de América', 'Egipto', 'El Salvador', 'Eritrea', 'Eslovaquia', 'Eslovenia',
+    'España', 'Estonia', 'Etiopía', 'Federación Rusa', 'Filipinas', 'Finlandia', 'Fiyi', 'Francia', 'Gabón', 'Gambia', 'Georgia', 'Ghana', 'Gibraltar',
+    'Granada', 'Grecia', 'Groenlandia', 'Guadalupe', 'Guam', 'Guatemala', 'Guayana Francesa', 'Guinea', 'Guinea Ecuatorial', 'Guinea-Bissau', 'Guyana',
+    'Haití', 'Honduras', 'Hong Kong', 'Hungría', 'India', 'Indonesia', 'Irán', 'Iraq', 'Irlanda', 'Islas Heard y Mcdonald', 'Islas Vírgenes (americanas)',
+    'Islas Marianas del Norte', 'Islas Sur Georgia y Sur Sandwich', 'Islas Turcas y Caicos', 'Islas Vírgenes GB', 'Isla Christmas', 'Islandia', 'Islas Bouvet',
+    'Islas Caimán', 'Islas Cocos', 'Islas Cook', 'Islas Feroe', 'Islas Malvinas', 'Islas Niue', 'Islas Norfolk', 'Islas Pitcairn', 'Islas Salomón', 'Islas Tokelau',
+    'Islas menores alejadas de EE.UU.', 'Israel', 'Italia', 'Jamaica', 'Japón', 'Jordania', 'Kazajistán', 'Kenia', 'Kirguizistán', 'Kiribati', 'Kuwait', 'Laos',
+    'Lesoto', 'Letonia', 'Líbano', 'Liberia', 'Libia', 'Liechtenstein', 'Lituania', 'Luxemburgo', 'Macao', 'Macedonia', 'Madagascar', 'Malasia', 'Malaui',
+    'Maldivas', 'Malí', 'Malta', 'Marruecos', 'Martinica', 'Mauricio (Isl.)', 'Mauritania', 'Mayotte', 'México', 'Micronesia', 'Moldavia', 'Mónaco', 'Mongolia',
+    'Montserrat', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Nicaragua', 'Níger', 'Nigeria', 'Noruega', 'Nueva Caledonia', 'Nueva Zelanda', 'Omán', 'Países Bajos',
+    'Pakistán', 'Palaos', 'Palestina', 'Panamá', 'Papúa Nueva Guinea', 'Paraguay', 'Perú', 'Polinesia francesa', 'Polonia', 'Portugal', 'Puerto Rico', 'Qatar', 'Reino Unido',
+    'República Centroafricana', 'República Dominicana', 'República Checa', 'República del Congo', 'República del Congo', 'Reunión', 'Ruanda', 'Rumanía', 'Saint Kitts y Nevis',
+    'St. Pedro y Miquelón', 'Santo Tomé y Príncipe', 'Sáhara occidental', 'Samoa americana', 'Samoa Occidental', 'San Marino', 'San Vicente y las Granadinas', 'Santa Helena',
+    'Santa Lucía', 'Senegal', 'Serbia y Montenegro', 'Seychelles', 'Sierra Leona', 'Singapur', 'Siria', 'Somalia', 'Sri Lanka', 'Suazilandia', 'Sudáfrica', 'Sudán', 'Suecia', 'Suiza',
+    'Surinam', 'Svalbard', 'Tailandia', 'Taiwan', 'Tanzania', 'Tayikistán', 'Timor oriental', 'Togo', 'Tonga', 'Trinidad y Tobago', 'Túnez', 'Turkmenistán', 'Turquía', 'Tuvalu',
+    'Ucrania', 'Uganda', 'Uruguay', 'Uzbekistán', 'Vanuatu', 'Venezuela', 'Vietnam', 'Wallis,Futuna', 'Yemen', 'Yibuti', 'Zambia', 'Zimbabue');
+?>
